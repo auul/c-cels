b8e084e77dfcf1f259131013d82b1dde1f5c0a6f
Alice Uu <alice@owl.ooo>
2026-02-06 09:22:27 -0800

    Add files via upload

---
+++ include/cel/pp/cat2.h
@@ -0,0 +1,6 @@
+#ifndef CEL_PP_CAT2
+
+#define CEL_PP_CAT2(a, b)      CEL_PP_CAT2__RAW(a, b)
+#define CEL_PP_CAT2__RAW(a, b) a##b
+
+#endif
---
+++ include/cel/pp/eval0.h
@@ -0,0 +1,5 @@
+#ifndef CEL_PP_EVAL0
+
+#define CEL_PP_EVAL0(x) x
+
+#endif
